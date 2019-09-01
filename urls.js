7e7fef71d2feee0e61137e08337723fe36e86639
Saioyan <35930420+Saioyan@users.noreply.github.com>
2019-09-01 16:30:55 +0800

    Update urls.js

--- urls.js
+++ urls.js
@@ -1,25 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+ <button id="btn" onclick="test()">点击按钮</button>
+	<br>
+<iframe src="https://reurl.cc/ao7XZ" id="iframe" width="250" height="1000"></iframe>
+</head>
+<body>
+ 
 <script>
-    function changeSrc()
-{
-document.getElementById("iframe").src="https://kk665403.pixnet.net/blog";
-}
-</script>
-<script src ='demo.js' type='text/javascript' data ='data'>
-var url = "..."
+    document.getElementById("btn").click();
+	var a = 0;
+	var b = 1;
+	var c = 0;
+	var urls = ""
+	function test(){
 function getRandom(min,max){
     return Math.floor(Math.random()*(max-min+1))+min;
 };
-a=getRandom(0,1);
+c=getRandom(0,1);
 
-function ABC(a){
-  if (a==0){
-  url="https://reurl.cc/ao7XZ"
+	insert(c);
+function insert(c){
+  if (c==0){
+document.getElementById("iframe").src="https://reurl.cc/ao7XZ";
   }
-  if (a==1){
-  url="http://www.codedata.com.tw/javascript/essential-javascript-variable/"
+    if (c==1){
+document.getElementById("iframe").src="https://kk665403.pixnet.net/blog";
   }
-  alert(url);
-};
-ABC(a);
+}
+		
+		
+	}
 </script>
-<iframe src="https://reurl.cc/ao7XZ" id="iframe" width="250" height="1000"></iframe>
+ 
+ 
+</body>
+</html>
