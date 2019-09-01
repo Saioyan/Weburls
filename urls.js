49f2c6253594e9a19a2dd6565c5c4882ebc39b2e
Saioyan <35930420+Saioyan@users.noreply.github.com>
2019-09-01 14:26:26 +0800

    Update urls.js

--- urls.js
+++ urls.js
@@ -1,1 +1,25 @@
-alert("123");
+<script>
+    function changeSrc()
+{
+document.getElementById("iframe").src="https://kk665403.pixnet.net/blog";
+}
+</script>
+<script src ='demo.js' type='text/javascript' data ='data'>
+var url = "..."
+function getRandom(min,max){
+    return Math.floor(Math.random()*(max-min+1))+min;
+};
+a=getRandom(0,1);
+
+function ABC(a){
+  if (a==0){
+  url="https://reurl.cc/ao7XZ"
+  }
+  if (a==1){
+  url="http://www.codedata.com.tw/javascript/essential-javascript-variable/"
+  }
+  alert(url);
+};
+ABC(a);
+</script>
+<iframe src="https://reurl.cc/ao7XZ" id="iframe" width="250" height="1000"></iframe>
