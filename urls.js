bb608ff2322b25762997ab5001da853d01c4fc8c
Saioyan <35930420+Saioyan@users.noreply.github.com>
2019-09-01 16:54:57 +0800

    Update urls.js

--- urls.js
+++ urls.js
@@ -1,29 +1,46 @@
-<button id="btn" onclick="test()" >123</button>
-<br>
-<iframe src="https://reurl.cc/ao7XZ" id="iframe" width="250" height="1000"></iframe>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
 
 <script>
-    document.getElementById("btn").click();
-	var a = 0;
-	var b = 1;
-	var c = 0;
-	var urls = ""
-	function test(){
+document.getElementById("btn").click();
+var urls = "..."
 function getRandom(min,max){
     return Math.floor(Math.random()*(max-min+1))+min;
 };
-c=getRandom(0,1);
+a=getRandom(0,1);
 
-	insert(c);
-function insert(c){
-  if (c==0){
-document.getElementById("iframe").src="https://reurl.cc/ao7XZ";
+function ABC(a){
+  if (a==0){
+  urls="https://reurl.cc/ao7XZ"
   }
-    if (c==1){
-document.getElementById("iframe").src="https://zi.media/@kk665403pixnetnetblog/post/yFWufI";
+  if (a==1){
+  urls="https://kk665403.pixnet.net/blog"
   }
+};
+ABC(a);
+function changeSrc(a){
+  if (a==0){
+  document.getElementById("iframe").src="https://reurl.cc/ao7XZ";
+  }
+    if (a==1){
+  document.getElementById("iframe").src="https://kk665403.pixnet.net/blog";
+  }
+
+document.getElementById("button").click();
 }
-		
-		
-	}
+
 </script>
+<input type="button" onclick="changeSrc(a)" value="Change Source">
+<br>
+
+
+<iframe src="https://reurl.cc/ao7XZ" id="iframe" width="250" height="1000"></iframe>
+
+</body>
+</html>
