91b235f35dc6233cb3826249a470eb3c14d74bd0
Saioyan <35930420+Saioyan@users.noreply.github.com>
2019-09-02 08:34:49 +0800

    Update urls.js

--- urls.js
+++ urls.js
@@ -25,8 +25,35 @@ function insert(c){
   if (c==0){
 document.getElementById("iframe").src="https://zi.media/@kk665403pixnetnetblog/post/XIBcLv";
   }
-   else{
+  else if(c==1){
 document.getElementById("iframe").src="https://zi.media/@kk665403pixnetnetblog/post/yFWufI";
+  }
+  else if(c==2){
+document.getElementById("iframe").src="https://zi.media/@kk665403pixnetnetblog/post/CP7sfk";
+  }
+  else if(c==3){
+document.getElementById("iframe").src="https://zi.media/@kk665403pixnetnetblog/post/n2ZWe2";
+  }
+	else if(c==4){
+document.getElementById("iframe").src="https://zi.media/@kk665403pixnetnetblog/post/qG8D42";
+  }
+	else if(c==5){
+document.getElementById("iframe").src="https://zi.media/@kk665403pixnetnetblog/post/w5obkk";
+  }
+	else if(c==6){
+document.getElementById("iframe").src="https://zi.media/@kk665403pixnetnetblog/post/LArm3y";
+  }
+	else if(c==7){
+document.getElementById("iframe").src="https://zi.media/@kk665403pixnetnetblog/post/7A5hIn";
+  }
+	else if(c==8){
+document.getElementById("iframe").src="https://zi.media/@kk665403pixnetnetblog/post/2kt3gH";
+  }
+	else if(c==9){
+document.getElementById("iframe").src="https://zi.media/@kk665403pixnetnetblog/post/PRzM9s";
+  }
+	else if(c==10){
+document.getElementById("iframe").src="https://zi.media/@kk665403pixnetnetblog/post/g5E2Ef";
   }
 }
 
