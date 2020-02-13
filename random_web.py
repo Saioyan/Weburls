4b065781fe1fe4cfc2da3d79c0de564bb3329f5c
Saioyan <35930420+Saioyan@users.noreply.github.com>
2020-02-13 16:03:55 +0800

    Update random_web.py

--- random_web.py
+++ random_web.py
@@ -1,72 +1,33 @@
-###########################################creat urls database
-import pandas as pd
 import random
-import sys,os
+import sys
 import tkinter as tk
 from tkinter import filedialog
-import subprocess
-infile = 'D:\\urldb\jfile.txt'
-output = 'D:\\urldb\\urls.txt'
-with open(infile,mode="r",encoding="utf-8") as file:
-    strr = file.read()
-a = len(strr)
-li=[]
-
-
-for i in range(0,a):
-#strr.find後面的網址修改為自己帳號的網址
-    num = strr.find('zi_url')
-    if num == -1:
-        break
-    if num > 0 :
-        num2 = strr.find('"status_code"')
-        strc = strr[num+10:num2+22]
-
-        if 'remov' not in strc or 'doin' not in strc :
-            li.append(strc)
-        strr = strr[num2+1:]
-
-print(li)
-str_url=''
-
-n=0
-for i in li:
-    if 'http' in i:
-        num = i.find('http')
-        num2 = i.find('",')
-        out = i[num:num2]
-        str_url += i + '\n'
-        n+=1
-print('have:',n,' urls in db')
-
-
-str_url = str_url.replace('\n\n','\n')
-str_url = str_url[:-1]
-fg = open(output, "w", encoding="utf-8")
-fg.write(str_url)
-li = ['urls','0','1','2','3']
-df = pd.DataFrame(pd.read_csv(output,sep='"',names=li))
-str_o=''
-for i in df['urls'] :
-    str_o += i +'\n'
-fg = open('D:\\urldb\\url_db.txt', "w", encoding="utf-8")
-fg.write(str_o)
-
-os.remove(output)
+import json
 ##############################################random urls
-root = tk.Tk()
-root.withdraw()
-file_path = filedialog.askopenfilename()
-num = file_path.rfind('/')
-fun = file_path[0:num]+'/function.txt'
-out = file_path[0:num]+'/output.txt'
+fun = 'D:\\urldb\\function.txt'
+out = 'D:\\urldb\\output.txt'
+with open("D:\\urldb\\jf.txt","r",encoding="utf-8") as fp:
+    stc=fp.read()
+j = json.loads(stc)
+n = 0
+m = 0
+strr=[]
+for i in j['data']:
+    url = i['zi_url']
+    stat = i['status_name']
+    if stat == '已同步':
+        strr.append(url)
+        m+=1
+    n+=1
+print('總篇數:',n)
+print('可抽取篇數:',m)
+
 def is_number(s):
     try:
         float(s)
         return True
     except ValueError:
         pass
-
     try:
         import unicodedata
         unicodedata.numeric(s)
@@ -77,24 +38,24 @@ def is_number(s):
     return False
 inum = input('輸入欲抽取數量:')
 if is_number(inum) == True:
-    print('Check input is number')
+    print('Running')
 else:
     print('Check input not number')
     sys.exit()
 h = int(inum)
-with open(file_path,"r",encoding="utf-8") as fp:
-    strr = fp.read()
-strr = strr.split("\n")
-strr.remove('')
+
+
 with open(fun,"r",encoding="utf-8") as fp:
     strr2 = fp.read()
 lenurl = len(strr)
 if h > lenurl :
     print('抽取數量大於庫存')
     sys.exit()
+
 dic1={}
 for i in range(0,lenurl):
     dic1.setdefault(i,strr[i])
+
 lenurl2 = lenurl
 li2=[]
 for i in range(0,h) :
@@ -102,11 +63,12 @@ for i in range(0,h) :
     li2.append(strr[c])
     strr.remove(strr[c])
     lenurl2 = len(strr)
+
+
 n=1
 body=''
 head = '    insert(c);'+'\n'+'function insert(c){'+'\n'+'    if (c==0){'+'\n'+'document.getElementById("iframe").src="https://zi.media/@kk665403pixnetnetblog/post/XIBcLv";'+'\n'+'}'+'\n'
 tail = '}'+'\n'+'</script>'+'\n'+'</body>'+'\n'+'</html>'
-
 for i in li2 :
     body += '    else if(c=='+str(n)+'){'+'\n'+'document.getElementById("iframe").src="'+str(i)+'";'+'\n'+'}'+'\n'
     n+=1
@@ -114,10 +76,5 @@ for i in li2 :
 s = 'c=getRandom(0,'+str(len(li2))+');'
 strr2 += '\n'+s+'\n'
 total = strr2 + head + body + tail
-print(total)
 fg = open(out, "w", encoding="utf-8")
 fg.write(total)
-
-
-
-
